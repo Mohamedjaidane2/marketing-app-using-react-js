f10bf03208d650a097c0bec55b1fa51c9106db7a
Mohamed <jaidanemohammed@gmail.com>
2022-10-02 18:39:13 +0200

    fix quotes responsive version

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -3,8 +3,8 @@ import React from "react";
 function Quotes() {
   return (
     <section className="bg-amber-400">
-      <div className=" items-center py-32 px-[10%] md:grid md:grid-cols-2 grid-flow-col md:gap-5">
-        <div className="font-normal text-[40px] ">
+      <div className=" items-center md:py-32 py-12 px-[10%] md:grid md:grid-cols-2 grid-flow-col md:gap-5">
+        <div className="font-normal md:text-[40px] text-[24px] ">
           <p className="">Avec le bon logiciel, de grandes choses peuvent se produire</p>
         </div>
         <div className="text-l pt-9 md:pt-0">
