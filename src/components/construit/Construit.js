970b8ada0f2a5cc3ed4df638c8145174c3d1a2f5
Mohamed <jaidanemohammed@gmail.com>
2022-10-04 10:02:17 +0200

    fix responsive

--- src/components/construit/Construit.js
+++ src/components/construit/Construit.js
@@ -7,19 +7,19 @@ import ct3 from "../../asstes/img/ct3.svg";
 function Construit() {
   return (
     <section className="px-[20%] py-[100px]">
-      <div className="grid lg:grid-cols-2 lg:gap-8">
+      <div className="grid grid-rows-2 lg:grid-cols-2 lg:gap-8">
         <div>
           <img className="h-auto w-[500px]" src={contruit} alt="construit" />
         </div>
         <div>
-          <h1 className="text-5xl leading-[62px]">Construit pour et par les créatifs</h1>
-          <p className="py-[30px] leading-8 text-xl" >
+          <h1 className="text-5xl text-center lg:text-left leading-[62px]">Construit pour et par les créatifs</h1>
+          <p className="py-[30px] leading-8 text-center lg:text-left text-xl" >
             Paragraphe. Vous pouvez le modifier et ajouter votre propre texte.
             Double-cliquez ici ou cliquez sur « Modifier le texte » pour ajouter
             votre contenu et personnaliser la police. C'est l'espace idéal pour
             raconter une histoire et vous présenter à vos visiteurs.
           </p>
-          <div className="grid grid-cols-3 gap-7 py-[30px]">
+          <div className="grid grid-rows-2  lg:grid-cols-3 gap-7 py-[30px]">
             <div className="flex space-x-4">
               <img className="w-[40px]" src={ct1} alt="ct" />
               <p>Kit d'outils complet</p>
