639ded6a7b2d0f91cccf9484dc1909b0091acea3
Mohamed <jaidanemohammed@gmail.com>
2022-11-05 00:24:35 +0100

    ..

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -3,7 +3,6 @@ import offer1 from "../../asstes/img/offer1.webp";
 import offer2 from "../../asstes/img/offer2.webp";
 import offer3 from "../../asstes/img/offer3.webp";
 import offer4 from "../../asstes/img/offer4.webp";
-import React, { Component }  from 'react';
 
 import "../../index.css";
 
